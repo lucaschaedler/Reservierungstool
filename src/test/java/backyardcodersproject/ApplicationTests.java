7cac411dbf48bcea507050b317c6b457a1dc8a1e
Luca Schädler <Luca Schdler@NB-Luca>
2021-04-09 11:34:24 +0200

    Projekt zu mavenprojekt geändert und bibliotheken erweitert, funktioniert jetzt grundlegend.

---
+++ src/test/java/backyardcodersproject/ApplicationTests.java
@@ -0,0 +1,13 @@
+package backyardcodersproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
