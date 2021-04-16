fe1a911cd4e10ed291d8750cf5f4a1cf6445378b
Luca Schädler <Luca Schdler@NB-Luca.home>
2021-04-16 14:30:31 +0200

    loggerklasse erstellt und Catenbank verbindung

--- src/main/java/backyardcodersproject/Application.java
+++ src/main/java/backyardcodersproject/Application.java
@@ -1,13 +1,23 @@
 package backyardcodersproject;
 
+import javax.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import Logger.LoggerClass;
 
 @SpringBootApplication
 public class Application {
 
+	LoggerClass logger = new LoggerClass();
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	 }
+	}
 
+	@PostConstruct
+	public void createHardCodedData() {
+		logger.getLogger().info(this.getClass().getName() + "||Application has successfully started||");
+
+	}
 }
