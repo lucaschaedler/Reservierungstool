58633bd2d132ac896192a080580a8acb43975536
Luca Schädler <Luca Schdler@NB-Luca.home>
2021-04-23 16:45:07 +0200

    login funktioniert

--- src/main/java/ch/mgmt/business/VerificationClass.java
+++ src/main/java/ch/mgmt/business/VerificationClass.java
@@ -1,14 +1,18 @@
 package ch.mgmt.business;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import ch.mgmt.logger.LoggerClass;
 import ch.mgmt.persistence.AccountRequest;
-
-
+import ch.mgmt.persistence.User;
+import ch.mgmt.persistence.UserRepository;
 
 @Service
 public class VerificationClass {
+	
+	@Autowired
+	UserRepository userRepository;
 	
 	LoggerClass logger = new LoggerClass();
 
@@ -18,6 +22,16 @@ public class VerificationClass {
 		
 		logger.getLogger().info(this.getClass().getName() + "||AccountRequest verified||");
 		return false;
+	}
+
+	public boolean VerifyLogin(String tempEmail, String tempPassword) {
+	User x = userRepository.findByUserEmailAndUserPassword(tempEmail, tempPassword);
+	if(x != null) {
+		logger.getLogger().info(this.getClass().getName() + "||User found||");
+		return true;
+	}else
+		return false;
+	
 	}
 	
 	
--- src/main/java/ch/mgmt/persistence/UserRepository.java
+++ src/main/java/ch/mgmt/persistence/UserRepository.java
@@ -4,8 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer>{
+public interface UserRepository extends JpaRepository<User, Integer> {
 
+	
+	User findByUserEmailAndUserPassword(String tempEmail, String tempPassword);
+		
 
 
 }
