e2adc34cfcbbbfdf91aab311e4492dfd154ffef5
Robin Roth <robin.roth@students.fhnw.ch>
2021-04-23 14:01:54 +0200

    getusers (service) funktioniert

--- src/main/java/ch/mgmt/service_user/UserCrudService.java
+++ src/main/java/ch/mgmt/service_user/UserCrudService.java
@@ -1,15 +1,21 @@
 package ch.mgmt.service_user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.mgmt.business.VerificationClass;
 import ch.mgmt.logger.LoggerClass;
+import ch.mgmt.persistence.User;
 import ch.mgmt.persistence.UserRepository;
 
 
 
 @RestController
+@RequestMapping("api/")
 public class UserCrudService {
 	
 	@Autowired
@@ -19,6 +25,11 @@ public class UserCrudService {
 	private VerificationClass verificationClass;
 	
 	LoggerClass logger = new LoggerClass();
+	
+	@GetMapping("users")
+	public List<User> getUsers(){
+		return this.userrepository.findAll();
+	}
 	
 
 }
