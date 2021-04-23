cb87fe0822fa4102ffab7e2704bd217123714d86
Robin Roth <robin.roth@students.fhnw.ch>
2021-04-23 15:36:23 +0200

    accountrequestcrudservice updated

--- src/main/java/ch/mgmt/service_account/AccountRequestCrudService.java
+++ src/main/java/ch/mgmt/service_account/AccountRequestCrudService.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import ch.mgmt.business.VerificationClass;
 import ch.mgmt.logger.LoggerClass;
+import ch.mgmt.messages.MessageNewAccountRequest;
 import ch.mgmt.persistence.AccountRequest;
 import ch.mgmt.persistence.AccountRequestRepository;
 
@@ -23,14 +24,14 @@ public class AccountRequestCrudService {
 	LoggerClass logger = new LoggerClass();
 	
 	@PostMapping(path = "/api/article/account_request", produces = "application/json")
-	public AccountRequest createAccountRequest(@RequestBody AccountRequest accountRequest) {
+	public AccountRequest createAccountRequest(@RequestBody MessageNewAccountRequest m) {
 		
 		AccountRequest a = new AccountRequest();
 
-		a.setAccountRequestEmail(accountRequest.getAccountRequestEmail());
-		a.setAccountRequestMobile(accountRequest.getAccountRequestMobile());
-		a.setAccountRequestName(accountRequest.getAccountRequestName());
-		a.setAccountRequestPassword(accountRequest.getAccountRequestPassword());
+		a.setAccountRequestEmail(m.getAccountRequestEmail());
+		a.setAccountRequestMobile(m.getAccountRequestMobile());
+		a.setAccountRequestName(m.getAccountRequestName());
+		a.setAccountRequestPassword(m.getAccountRequestPassword());
 		if (verificationClass.validateAccountRequest(a)) {
 			accountRepository.save(a);
 			logger.getLogger().info(this.getClass().getName() + "||AccountRequest created||");
